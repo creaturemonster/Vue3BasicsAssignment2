ff177101a6a0428bea783cd157a86413545ca741
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-09-24 23:11:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 10,
+      name:'', 
+      confirmedName:''
+    };
+  },
+  methods:{
+    add(num){
+      this.counter=this.counter+num;
+    }, 
+    reduce(num){
+      this.counter=this.counter-num;
+    }, 
+    setName(event, lastName){
+      this.name=event.target.value + " " + lastName;
+    }, 
+    submitForm(){
+      alert('Submitted!');
+    }, 
+    confirmedInput(){
+      this.confirmedName=this.name;
+    }
+  }
+});
 
+app.mount('#events');
